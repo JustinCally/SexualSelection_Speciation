ad680653e8793fcb4dafe6ee4c199566d0f71a17
Justin Cally <jcally@student.unimelb.edu.au>
2018-06-27 11:42:20 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SS_Extinction
+A repository for the comparative study on sexual selection and extinction rate in birds.
---
+++ bamm_extinction/functions/check_and_fix_ultrametric.R
@@ -0,0 +1,17 @@
+check_and_fix_ultrametric <- function(phy){
+  
+  if (!is.ultrametric(phy)){
+    
+    vv <- vcv.phylo(phy)
+    dx <- diag(vv)
+    mxx <- max(dx) - dx
+    for (i in 1:length(mxx)){
+      phy$edge.length[phy$edge[,2] == i] <- phy$edge.length[phy$edge[,2] == i] + mxx[i]
+    }
+    if (!is.ultrametric(phy)){
+      stop("Ultrametric fix failed\n")
+    }	
+  }
+  
+  return(phy)
+}
